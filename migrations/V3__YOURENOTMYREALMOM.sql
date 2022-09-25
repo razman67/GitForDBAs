03169eab2ac88a3a877f16faa0f2f1b7af155759
Eric Rasinski <eric.rasinski@gmail.com>
2022-09-25 16:47:27 -0500

    compose and updates

--- migrations/V3__YOURENOTMYREALMOM.sql
+++ migrations/V3__YOURENOTMYREALMOM.sql
@@ -1,5 +1,5 @@
 CREATE TABLE dbo.YoureNotMyRealMom(    
      YoureNotMyRealMomId INT IDENTITY NOT NULL,    
      Declaration nvarchar(1000) NULL,    
-     CONSTRAINT PK_YoureNotMyRealMom PRIMARY KEY CLUSTERED (YoureNotMyRealMom)
+     CONSTRAINT PK_YoureNotMyRealMom PRIMARY KEY CLUSTERED (YoureNotMyRealMomID)
 );
